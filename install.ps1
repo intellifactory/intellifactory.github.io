12e295dd0752cbe5d44e6fd529e00adb7165700a
jooseppi12 <urijozsef@europe.com>
2021-02-02 00:21:14 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,6 +2,14 @@ param(
     [switch] $buildOnly
 )
 
+echo "Copy legal files"
+
+xcopy .\legal\site-docs\intellifactory.com\* .\src\Hosted\legal\ /s /e
+
+echo "Copy blog posts files"
+
+xcopy .\blogs\user\* .\src\Hosted\posts\ /s /e
+
 # Initialize git submodules
 git submodule update --init --recursive
 
